8f9b99465334d19df18bb31a521cc3c736531061
Félicien Brochu <felicien.brochu@gmail.com>
2018-10-06 18:06:36 +0200

    Game Menu navigation.

--- src/goclock/GameUiHandler.h
+++ src/goclock/GameUiHandler.h
@@ -9,6 +9,13 @@
 #include "GameButtonGestures.h"
 #include "Buzzer.h"
 
+#define MENU_ITEM_GAME 0
+#define MENU_ITEM_EXIT 1
+#define MENU_ITEM_ADD_TIME 2
+#define MENU_ITEM_RESTART 3
+
+#define MENU_ITEM_COUNT 4
+
 extern GameClock *gameClock;
 extern GameButtonGestures buttonGestures;
 extern GameClockLcd lcd2;
@@ -19,39 +26,68 @@ extern UiHandler *startingHandler;
 const char gameUiHandlerPlayerOneWinnerMessage[] PROGMEM = "WINNER ";
 const char gameUiHandlerPlayerTwoWinnerMessage[] PROGMEM = " WINNER";
 
+const char gameUiHandlerExitItem[] PROGMEM = "End Game";
+const char gameUiHandlerAddTimeItem[] PROGMEM = "Add time";
+const char gameUiHandlerRestartItem[] PROGMEM = "Restart Game";
+
 class GameUiHandler : public UiHandler {
 	TimeControlUi *timeControlUi;
 	bool started;
 	int8_t toggleButtonState;
+	int menuIndex;
 
 public:
 
 	GameUiHandler() {
 		started           = false;
 		toggleButtonState = -1;
+		menuIndex         = MENU_ITEM_GAME;
 	}
 
 	virtual void tick(Clock *clock) {
 		buttonGestures.tick(clock);
+		bool buttonPushed = buttonGestures.wasPushButtonPushed();
+		int  travel       = buttonGestures.getNavigationTravel();
 
-		if (started) {
-			gameClock->tick();
+		if (buttonGestures.wasPushButtonLongPushed()) {
+			goBackToStartingHandler();
+			return;
+		}
 
-			if (buttonGestures.wasPushButtonLongPushed()) {
-				goBackToStartingHandler();
-				return;
+		if (!started && (menuIndex == MENU_ITEM_GAME)) { // < Initialization
+			if (toggleButtonState == -1) {
+				lcd2.setBlinking(true);
+				toggleButtonState = buttonGestures.isToggleButtonDisabled();
+			} else if (toggleButtonState != buttonGestures.isToggleButtonDisabled()) {
+				lcd2.setBlinking(false);
+				started = true;
+			}
+		}
+
+		if (!started || gameClock->isPaused() || gameClock->isOver()) { // < Menu
+			if (travel != 0) {
+				menuIndex = (menuIndex + travel) % MENU_ITEM_COUNT;
+
+				if (menuIndex < 0) {
+					menuIndex = MENU_ITEM_COUNT + menuIndex;
+				}
+
+				if ((menuIndex == MENU_ITEM_GAME) && (gameClock->isPaused() || !started)) {
+					lcd2.setBlinking(true);
+				} else {
+					lcd2.setBlinking(false);
+				}
 			}
 
-			if (buttonGestures.wasPushButtonPushed()) {
-				if (gameClock->isOver()) {
-					goBackToStartingHandler();
-				} else if (gameClock->isPaused()) {
-					lcd2.setBlinking(false);
-					gameClock->resume();
-				} else {
-					lcd2.setBlinking(true);
-					gameClock->pause();
-				}
+			if (buttonPushed) {
+				onMenuClick();
+			}
+		} else if ((menuIndex == MENU_ITEM_GAME) && started && !gameClock->isPaused() && !gameClock->isOver()) { // < Normal play
+			gameClock->tick();
+
+			if (buttonPushed) {
+				lcd2.setBlinking(true);
+				gameClock->pause();
 			}
 
 			if (buttonGestures.isToggleButtonDisabled()) {
@@ -59,18 +95,34 @@ public:
 			} else if (buttonGestures.isToggleButtonEnabled()) {
 				gameClock->selectPlayerTwo();
 			}
-		} else {
-			if ((toggleButtonState >= 0) &&
-			    (toggleButtonState != buttonGestures.isToggleButtonDisabled())) {
-				started = true;
-			} else {
-				toggleButtonState = buttonGestures.isToggleButtonDisabled();
-			}
 		}
 	}
 
 	virtual void render(Clock *clock) {
 		lcd2.beginRender(clock);
+
+		switch (menuIndex) {
+		case MENU_ITEM_GAME:
+			renderGame(clock);
+			break;
+
+		case MENU_ITEM_EXIT:
+			renderExitItem(clock);
+			break;
+
+		case MENU_ITEM_ADD_TIME:
+			renderAddTimeItem(clock);
+			break;
+
+		case MENU_ITEM_RESTART:
+			renderRestartItem(clock);
+			break;
+		}
+
+		lcd2.endRender();
+	}
+
+	void renderGame(Clock *clock) {
 		bool doBeep = timeControlUi->renderGame(gameClock, &lcd2);
 
 		if (doBeep) {
@@ -84,13 +136,25 @@ public:
 				lcd2.printBottomRight(gameUiHandlerPlayerTwoWinnerMessage);
 			}
 		}
-		lcd2.endRender();
+	}
+
+	void renderExitItem(Clock *clock) {
+		lcd2.printTopCenter(gameUiHandlerExitItem);
+	}
+
+	void renderAddTimeItem(Clock *clock) {
+		lcd2.printTopCenter(gameUiHandlerAddTimeItem);
+	}
+
+	void renderRestartItem(Clock *clock) {
+		lcd2.printTopCenter(gameUiHandlerRestartItem);
 	}
 
 	void setup(TimeControlUi *timeControlUi) {
 		this->timeControlUi = timeControlUi;
 		started             = false;
 		toggleButtonState   = -1;
+		menuIndex           = MENU_ITEM_GAME;
 	}
 
 private:
@@ -100,6 +164,39 @@ private:
 		lcd2.setBlinking(false);
 		delete gameClock;
 	}
+
+	void onMenuClick() {
+		switch (menuIndex) {
+		case MENU_ITEM_GAME:
+			onGameClick();
+			break;
+
+		case MENU_ITEM_EXIT:
+			goBackToStartingHandler();
+			break;
+
+		case MENU_ITEM_ADD_TIME:
+			onAddTimeClick();
+			break;
+
+		case MENU_ITEM_RESTART:
+			onRestartClick();
+			break;
+		}
+	}
+
+	void onGameClick() {
+		if (gameClock->isOver()) {
+			goBackToStartingHandler();
+		} else if (gameClock->isPaused()) {
+			lcd2.setBlinking(false);
+			gameClock->resume();
+		}
+	}
+
+	void onAddTimeClick() {}
+
+	void onRestartClick() {}
 };
 
 #endif // ifndef __GameUiHandler_h__
